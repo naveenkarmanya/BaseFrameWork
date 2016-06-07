049546b349bf530ccbd8e19828160b7b90acf8cc
naveen <naveen.goud@karmanya.co.in>
2016-06-07 16:04:25 +0530

    base framework

---
+++ app/Http/Controllers/AdminLTEController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class AuthController extends Controller
+{
+    public function admin() {
+
+        return view('welcome');
+    }
+
+}
